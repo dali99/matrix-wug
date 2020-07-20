1a4245b158c0386b40e67059894449e54628e696
Daniel Olsen <daniel.olsen99@gmail.com>
2020-07-20 02:12:17 +0200

    switch to yarn

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "node";
-  buildInputs = [ nodejs-10_x ];
+  buildInputs = [ nodejs-10_x yarn ];
   shellHook = ''
     export PATH="$PWD/node_modules/.bin/:$PATH"
   '';
